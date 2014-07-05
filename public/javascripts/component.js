785ad65bd8640e65267b3d6552492664b575cdd4
Prescott Murphy <prurph@gmail.com>
2014-07-04 20:31:53 -0400

    Add some basic React.js components

---
+++ public/javascripts/component.js
@@ -0,0 +1,51 @@
+/** @jsx React.DOM */
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Bilinda">Tell me all your favorite things.</Comment>
+        <Comment author="Prescott">Tell me all yours too?</Comment>
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+React.renderComponent(
+  <CommentBox />,
+  document.getElementById('content')
+);
